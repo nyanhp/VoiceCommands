49bb4e2f52ba38fd8cd52e57ddbca0f74cbf7cc8
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2017-11-09 21:52:38 +0100

    Initial commit of VoiceCommands

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # VoiceCommands
+A little module that allows speech output. Might be extended in the future.
+
+# Example
+Using Out-Voice is simple enough. Just pipe stuff to it.
+```powershell  
+"PowerShell is awesome!" | Out-Voice
+```  
+
+Optionally you can select a gender. Out-Voice defaults to female.  
+```powershell
+"3...2...1... Engange!" | Out-Voice -Gender Male
+```
---
+++ VoiceCommands/VoiceCommands.psm1
@@ -0,0 +1,33 @@
+﻿function Out-Voice
+{
+param
+(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [System.String]
+    $Text,
+
+    [ValidateSet('Male','Female')]
+    [System.String]
+    $Gender = 'Female'
+)
+
+begin
+{
+    $synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $synth.SelectVoiceByHints($Gender)
+    $synth.SetOutputToDefaultAudioDevice()
+}
+
+process
+{  
+    $synth.Speak($Text)
+
+}
+
+end
+{
+    $synth.Dispose()
+}
+}
+
+New-Alias -Name ov -Value Out-Voice -Description "Alias for voice cmdlet Out-Voice" -Force
